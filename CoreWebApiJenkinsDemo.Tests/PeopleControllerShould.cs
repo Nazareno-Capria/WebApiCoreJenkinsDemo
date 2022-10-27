b561d916a39e1c7cf38fcb5c27c90b5520e31e7b
Nazareno-Capria <nazarenocapria@gmail.com>
2022-10-27 16:05:06 -0300

    first commit

---
+++ CoreWebApiJenkinsDemo.Tests/PeopleControllerShould.cs
@@ -0,0 +1,26 @@
+﻿using CoreWebApiJenkinsDemo.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CoreWebApiJenkinsDemo.Tests
+{
+    public class PeopleControllerShould
+    {
+        private readonly PeopleController _peopleController;
+        public PeopleControllerShould()
+        {
+            _peopleController = new PeopleController();
+        }
+        [Fact]
+        public void People_InputIsString_ReturnTrue()
+        {
+            var stringResult = _peopleController.GetName();
+
+            Assert.IsType<string>(stringResult);
+        }
+    }
+}
---
+++ CoreWebApiJenkinsDemo/Controllers/PeopleController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreWebApiJenkinsDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PeopleController : ControllerBase
+    {
+
+        private static string[] Names = {"Nazareno", "Adrian", "Jose", "Raul", "Julito" };
+
+
+        public PeopleController()
+        {
+
+        }
+
+
+        [HttpGet]
+        public string GetName()
+        {
+            return Names[Random.Shared.Next(Names.Length)];
+        }
+    }
+}
